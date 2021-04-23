d2cdd92f5537403caaca2f56fd8e4f4a10d8fb59
trenmcki071 <trenmcki071@outlook.com>
2021-04-23 13:49:42 -0400

    Initial Commit

--- Play_Button/Form1.cs
+++ Play_Button/Form1.cs
@@ -27,6 +27,7 @@ namespace Play_Button
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
+            //button sound, reveals countdown, hides button
             SoundPlayer player3 = new SoundPlayer(Properties.Resources.Click);
             player3.PlaySync();
             Thread.Sleep(100);
@@ -34,23 +35,25 @@ namespace Play_Button
             timerNumber.ForeColor = Color.White;
             buttonStart.Visible = false;
 
+            //timer noise
             Refresh();
             SoundPlayer player = new SoundPlayer(Properties.Resources.Ping);
             player.Play();
             Thread.Sleep(1000);
-
             timerNumber.Text = "2";
 
+            //timer noise
             Refresh();
             player.Play();
             Thread.Sleep(1000);
-
             timerNumber.Text = "1";
 
+            //timer noise
             Refresh();
             player.Play();
             Thread.Sleep(1000);
 
+            //start noise, background color change, hides countdown, says GO!!!!
             SoundPlayer player2 = new SoundPlayer(Properties.Resources.Start);
             background.BackColor = Color.Green;
             player2.Play();
